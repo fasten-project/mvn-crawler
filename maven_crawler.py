7e9008d8db4c6e2b5a1c95709aa5af0a2ee7f5f8
mir-am <mir-am@hotmail.com>
2019-12-12 17:22:05 +0100

    A non-recursive approach to get the POM files.

--- maven_crawler.py
+++ maven_crawler.py
@@ -23,6 +23,7 @@ from kafka import KafkaProducer
 from urllib.request import urlopen
 from urllib.parse import urlparse, urljoin
 from os.path import split, exists, join, isfile
+from collections import deque
 from os import makedirs
 from datetime import datetime
 import re
@@ -70,17 +71,37 @@ class MavenCoordProducer:
         print(excp)
 
 
+class PageLink:
+    """
+    Encapsulates a page link with its url, file hierarchy, and timestamp
+    """
+
+    def __init__(self, url, file_h, timestamp):
+        self.url = url
+        self.file_h = file_h
+        self.timestamp = timestamp
+
+
+def url_last_part(url):
+    """
+    Returns last part of a URL.
+    E.g. http://repo2.maven.apache.org/maven2/HTTPClient/HTTPClient/0.3-3/HTTPClient-0.3-3.pom -> HTTPClient-0.3-3.pom
+    :param url:
+    :return:
+    """
+    return split(urlparse(url).path)[-1]
+
+
 def is_url_file(url):
     """
     Checks whether the url is file or not.
     :param url:
     :return:
     """
-    path = urlparse(url).path
-    return bool(re.match(r".+\.\w+", split(path)[-1]))
+    return bool(re.match(r".+\.\w+", url_last_part(url)))
 
 
-def download_pom(url, pom_file_name, path):
+def download_pom(url, path):
     """
     It downloads a POM file in the specified path
     :param url:
@@ -90,7 +111,7 @@ def download_pom(url, pom_file_name, path):
     """
 
     response = requests.get(url)
-    with open(join(path, pom_file_name), 'wb') as pom_f:
+    with open(path, 'wb') as pom_f:
         pom_f.write(response.content)
 
 
@@ -125,67 +146,148 @@ def process_pom_file(path):
     return {'groupId': group_id, 'artifactId': artifact_id, 'version': version, 'date': ''}
 
 
-def extract_pom_files(url, dest, next_sibling, cooldown, mvn_coord_producer):
+# def extract_pom_files(url, dest, next_sibling, cooldown, mvn_coord_producer):
+#     """
+#     It finds and downloads all the POM files same as the Maven repositories' hierarchy.
+#     :param url:
+#     :param dest: The path to save the maven dirs
+#     :param next_sibling:
+#     :param cooldown:
+#     :param kafka_producer: An instance of MavenCoordProducer
+#     :return:
+#     """
+#
+#     # Wait...
+#     time.sleep(cooldown)
+#
+#     page_content = urlopen(url).read()
+#     soup = BeautifulSoup(page_content, 'html.parser')
+#
+#     # Skips the up level dir (e.g. ../)
+#     for i, a in enumerate(soup.find_all('a', href=True)[1:]):
+#
+#         if i <= 5:
+#
+#             if not is_url_file(a['href']):
+#
+#                 # Create directories same as the Maven's hierarchy
+#                 if not exists(join(dest, dest, a['href'])):
+#                     makedirs(join(dest, dest, a['href']))
+#
+#                 #print("Found the url", a['href'])
+#                 extract_pom_files(urljoin(url, a['href']), join(dest, a['href']), a.next_sibling, cooldown, mvn_coord_producer)
+#
+#             elif bool(re.match(r".+\.pom$", a['href'])):
+#                 #print("Found a POM file: ", a['href'])
+#                 #print("Timestamp: ", next_sibling.split())
+#
+#                 # Checks whether the POM file is already downloaded.
+#                 if not isfile(join(dest, dest, a['href'])):
+#
+#                     download_pom(urljoin(url, a['href']), a['href'], join(dest, dest))
+#                     print("Downloaded %s" % join(dest, dest, a['href']))
+#
+#                 else:
+#                     print("File %s exits." % join(dest, dest, a['href']))
+#
+#                 mvn_coords = process_pom_file(join(dest, dest, a['href']))
+#
+#                 # TODO: For some projects, timestamp is not retrieved properly!
+#                 timestamp = next_sibling.split()
+#                 mvn_coords['date'] = timestamp[0] + " " + timestamp[1]
+#
+#                 if mvn_coords['date'] != "- -":
+#                     mvn_coords['date'] = str(convert_to_unix_epoch(mvn_coords['date']))
+#                     print(mvn_coords['date'])
+#                     mvn_coord_producer.put(mvn_coords)
+#
+#                 #csv_writer[0].writerow([gid, aid, ver, timestamp[0] + " " + timestamp[1]])
+#                 #csv_writer[1].flush()
+#         else:
+#             break
+#
+#         mvn_coord_producer.kafka_producer.flush()
+
+
+def extract_pom_files(url, dest, cooldown, mvn_coord_producer):
     """
-    It finds and downloads all the POM files same as the Maven repositories' hierarchy.
+    Extracts the POM files from given maven repository and puts them in a Kafka topic. It is a non-recursive approach
+    and uses a queue.
     :param url:
-    :param dest: The path to save the maven dirs
+    :param dest:
     :param next_sibling:
     :param cooldown:
-    :param kafka_producer: An instance of MavenCoordProducer
+    :param mvn_coord_producer: An instance of MavenCoordProducer
     :return:
     """
 
-    # Wait...
-    time.sleep(cooldown)
+    # A queue
+    q = deque([PageLink(urljoin(url, u['href']), u['href'], u.next_sibling.strip()) for u in extract_page_links(url)[1:]])
+
+    r = 1
+    j = 0
+
+    while len(q) != 0 and j <= 5:
+
+        # Picks one item from the beginning of the queue
+        u = q.popleft()
+        print("URL:", u.url, " | ", "FH: ", u.file_h, " | ", "TS: ", u.timestamp)
+        if not is_url_file(u.url):
+            #print("Path: ", join(MVN_PATH, u.file_h))
+            # Create directories same as the Maven's hierarchy
+            if not exists(join(dest, u.file_h)):
+                makedirs(join(dest, u.file_h))
+
+            # Wait before sending a request
+            time.sleep(cooldown)
+
+            r += 1
+            # Skips the up-level dir. (e.g. \..)
+            for pl in extract_page_links(u.url)[1:]:
+                #print("URL:", urljoin(u.url, pl['href']), "FH: ", join(u.file_h, pl['href']), "TS: ", pl.next_sibling.strip())
+
+                q.appendleft(PageLink(urljoin(u.url, pl['href']), join(u.file_h, pl['href']), pl.next_sibling.strip()))
+
+        elif bool(re.match(r".+\.pom$", u.url)):
+            print("Found a POM file: ", u.url)
+            j += 1
+
+            # Checks whether the POM file is already downloaded.
+            if not isfile(join(dest, u.file_h)):
+
+                download_pom(u.url, join(dest, u.file_h))
+                print("Downloaded %s" % join(dest, u.file_h))
+
+            else:
+                print("File %s exits." % join(dest, u.file_h))
+
+            mvn_coords = process_pom_file(join(dest, u.file_h))
+
+            # TODO: For some projects, timestamp is not retrieved properly!
+            timestamp = u.timestamp.split()
+            mvn_coords['date'] = timestamp[0] + " " + timestamp[1]
+            print(mvn_coords['date'])
+
+            if mvn_coords['date'] != "- -":
+                mvn_coords['date'] = str(convert_to_unix_epoch(mvn_coords['date']))
+                print(mvn_coords['date'])
+                mvn_coord_producer.put(mvn_coords)
+
+        print("R: ", r)
+
+    mvn_coord_producer.kafka_producer.flush()
+
+
+def extract_page_links(url):
+    """
+    Extracts all the links in a web page.
+    :param url:
+    :return:
+    """
 
     page_content = urlopen(url).read()
     soup = BeautifulSoup(page_content, 'html.parser')
-
-    # Skips the up level dir (e.g. ../)
-    for i, a in enumerate(soup.find_all('a', href=True)[1:]):
-
-        if i <= 5:
-
-            if not is_url_file(a['href']):
-
-                # Create directories same as the Maven's hierarchy
-                if not exists(join(MVN_PATH, dest, a['href'])):
-                    makedirs(join(MVN_PATH, dest, a['href']))
-
-                #print("Found the url", a['href'])
-                extract_pom_files(urljoin(url, a['href']), join(dest, a['href']), a.next_sibling, cooldown, mvn_coord_producer)
-
-            elif bool(re.match(r".+\.pom$", a['href'])):
-                #print("Found a POM file: ", a['href'])
-                #print("Timestamp: ", next_sibling.split())
-
-                # Checks whether the POM file is already downloaded.
-                if not isfile(join(MVN_PATH, dest, a['href'])):
-
-                    download_pom(urljoin(url, a['href']), a['href'], join(MVN_PATH, dest))
-                    print("Downloaded %s" % join(MVN_PATH, dest, a['href']))
-
-                else:
-                    print("File %s exits." % join(MVN_PATH, dest, a['href']))
-
-                mvn_coords = process_pom_file(join(MVN_PATH, dest, a['href']))
-
-                # TODO: For some projects, timestamp is not retrieved properly!
-                timestamp = next_sibling.split()
-                mvn_coords['date'] = timestamp[0] + " " + timestamp[1]
-
-                if mvn_coords['date'] != "- -":
-                    mvn_coords['date'] = str(convert_to_unix_epoch(mvn_coords['date']))
-                    print(mvn_coords['date'])
-                    mvn_coord_producer.put(mvn_coords)
-
-                #csv_writer[0].writerow([gid, aid, ver, timestamp[0] + " " + timestamp[1]])
-                #csv_writer[1].flush()
-        else:
-            break
-
-        mvn_coord_producer.kafka_producer.flush()
+    return soup.find_all('a', href=True)
 
 
 if __name__ == '__main__':
@@ -201,4 +303,5 @@ if __name__ == '__main__':
 
     mvn_producer = MavenCoordProducer(args.h)
 
-    extract_pom_files(args.m, '', None, args.c, mvn_producer)
+    #extract_pom_files(args.m, '', None, args.c, mvn_producer)
+    extract_pom_files(args.m, MVN_PATH, args.c, mvn_producer)
